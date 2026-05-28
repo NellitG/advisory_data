4acb9be4ac20c71efba31945741985cbecc82d9c
NellitG <kamaunelson157@gmail.com>
2026-05-28 14:34:39 +0300

    Added str method to class to provide a string representation of an instance with value_chain and question attributes

--- data/models.py
+++ data/models.py
@@ -5,3 +5,6 @@ class AdvisoryKnowledge(models.Model):
     value_chain = models.CharField(max_length=100)
     question = models.TextField()
     answer = models.TextField()
+
+    def __str__(self):
+        return f"{self.value_chain}: {self.question[:80]}"
